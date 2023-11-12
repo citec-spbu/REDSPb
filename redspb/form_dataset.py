3dfab39903c54ed8aeffe0f3d58403ca88d4d8dc
Alexander Allakverdyan <aleka_alexander@mail.ru>
2023-11-12 20:36:50 +0300

    feat: add examples for learning models

--- redspb/form_dataset.py
+++ redspb/form_dataset.py
@@ -13,10 +13,14 @@ def generate_dataset(dataset: pd.DataFrame, n: int = 10, m: int = 100, step_n: i
     :param step_n: height window step
     :param step_m: width window step
     :param info: if True -> prints sizes of data in GBs
-    :return: pair of scaled X and Y
+    :return: pair of scaled X and Y and true shape
     """
     matrix_numbers = np.array(dataset['numbers'].values.tolist(), dtype=float)
     matrix_target = np.array(dataset['target'].values.tolist(), dtype=float)
+    mkm_0 = matrix_numbers.shape[0]
+    mkm_1 = matrix_numbers.shape[1]
+    matrix_numbers = matrix_numbers[:-(mkm_0 % n), :-(mkm_1 % m)]
+    matrix_target = matrix_target[:-(mkm_0 % n), :-(mkm_1 % m)]
 
     scaler = MinMaxScaler()
     matrix_numbers = scaler.fit_transform(matrix_numbers)
@@ -25,8 +29,12 @@ def generate_dataset(dataset: pd.DataFrame, n: int = 10, m: int = 100, step_n: i
         print('Memory size was:', matrix_numbers.nbytes / 2**30, 'GB, ', matrix_target.nbytes / 2**30, 'GB')
 
     x = []
+    counter_0 = 0
     for i in tqdm(range(0, matrix_numbers.shape[0] - n + 1, step_n)):
+        counter_0 += 1
+        counter_1 = 0
         for j in range(0, matrix_numbers.shape[1] - m + 1, step_m):
+            counter_1 += 1
             window = matrix_numbers[i:i + n, j:j + m]
             x.append(window)
     x = np.array(x, dtype=float)
@@ -40,13 +48,13 @@ def generate_dataset(dataset: pd.DataFrame, n: int = 10, m: int = 100, step_n: i
 
     if info:
         print('Memory size X Y:', x.nbytes / 2**30, 'GB, ', y.nbytes / 2**30, 'GB')
-    return x, y
+    return x, y, (counter_0, counter_1)
 
 
 # df = pd.read_csv("/Users/aleksandrallahverdan/Downloads/data1.csv")
 # df['numbers'] = df['numbers'].map(lambda x: list(map(float, x[1:-1].split(','))))
 # df['target'] = df['target'].map(lambda x: list(map(float, x.split(','))))
-#
-# X, Y = generate_dataset(df, info=True)
-#
-# print(X.shape, Y.shape)
+
+# X, Y, sh = generate_dataset(df, info=True)
+
+# print(X.shape, Y.shape, sh)
---
+++ redspb/learn_example.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+import psutil
+from keras.layers import Conv2D, Flatten, Dense, Conv2DTranspose, Input, Conv1D, InputLayer, BatchNormalization
+from keras.models import Model, Sequential
+# from keras.optimizers import Adam
+from form_dataset import generate_dataset
+from tensorflow.keras.optimizers.legacy import Adam
+
+df = pd.read_csv("/Users/aleksandrallahverdan/Downloads/part1.csv")
+df['numbers'] = df['numbers'].map(lambda x: list(map(float, x[1:-1].split(','))))
+df['target'] = df['target'].map(lambda x: list(map(float, x.split(','))))
+X, Y, true_raw_shape = generate_dataset(df, info=True)
+del df
+
+def train_test_split(X, Y, test_size=0.2):
+    assert 0 < test_size < 1
+    n = int(X.shape[0] * (1-test_size))
+    return X[:n], X[n:], Y[:n], Y[n:]
+
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.4)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+del X
+del Y
+
+
+# Архитектура сети
+input_shape = (10, 100, 1)
+inputs = Input(shape=input_shape)
+x = Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+x = BatchNormalization()(x)
+x = Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+outputs = Conv2D(1, kernel_size=(3, 3), activation='linear', padding='same')(x)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001))
+model.fit(x_train, y_train, batch_size=512, epochs=3, validation_split=0.2)
+
+predicted_Y = model.predict(x_test)
+
+
+def reconstruct_matrix_super(matrix_target_shape, y, n=10, step_n=10, step_m=10):
+    res = []
+    i = 0
+    while len(y) > matrix_target_shape[1]*(i+1):
+        layer = y[matrix_target_shape[1]*i:matrix_target_shape[1]*(i+1):step_m]
+        layer = np.concatenate(layer, axis=1)
+        res.append(layer)
+        i += n // step_n
+    res = np.concatenate(res, axis=0)
+    return res
+
+
+
+
+rec1 = reconstruct_matrix_super(true_raw_shape, y_test)
+rec2 = reconstruct_matrix_super(true_raw_shape, predicted_Y)
+
+
+plt.subplot(2, 1, 1)  # 2 строки, 1 столбец, подграфик 1
+plt.imshow(rec1, cmap='hot')  # График для первого подграфика
+plt.title('Subplot 1')  # Заголовок для первого подграфика
+
+# Создание второго подграфика
+plt.subplot(2, 1, 2)  # 2 строки, 1 столбец, подграфик 2
+plt.imshow(rec2, cmap='hot')  # График для второго подграфика
+plt.title('Subplot 2')  # Заголовок для второго подграфика
+
+# Отображение графиков
+plt.tight_layout()  # Автоматическое расположение подграфиков
+plt.show()
