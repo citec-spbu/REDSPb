c7fb9120a0b01ba90014e9dfb04e24bf4a0a751e
Albina <albinacneg55@gmail.com>
2023-11-19 01:01:25 +0300

    add data separator

---
+++ redspb/halfdata.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+path_to_file = "D:\practice\data1.csv"
+df = pd.read_csv(path_to_file)
+
+split_index = len(df) // 2
+df1 = df.iloc[:split_index]
+df2 = df.iloc[split_index:]
+
+df1.to_csv(path_to_file, index=False)
+df2.to_csv(path_to_file, index=False)
