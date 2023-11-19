58453361cd02758cafd4b0dc9516012c67ae729b
Alexander Allakverdyan <aleka_alexander@mail.ru>
2023-11-19 21:56:59 +0300

    feat: update scaler

--- redspb/learn_example.py
+++ redspb/learn_example.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from keras.src.layers import MaxPooling2D
+from matplotlib import widgets
 from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -11,7 +13,7 @@ from keras.models import Model, Sequential
 from form_dataset import generate_dataset
 from tensorflow.keras.optimizers.legacy import Adam
 
-df = pd.read_csv("/Users/aleksandrallahverdan/Downloads/part1.csv")
+df = pd.read_csv("/Users/aleksandrallahverdan/Downloads/data_res_2_half_upd.csv")
 df['numbers'] = df['numbers'].map(lambda x: list(map(float, x[1:-1].split(','))))
 df['target'] = df['target'].map(lambda x: list(map(float, x.split(','))))
 X, Y, true_raw_shape = generate_dataset(df, info=True)
@@ -32,19 +34,32 @@ del X
 del Y
 
 
+# def min_max_scaler(arr: np.ndarray):
+#     return (arr - arr.min()) / (arr.max() - arr.min())
+#
+#
+# for i in range(len(x_train)):
+#     x_train[i] = min_max_scaler(x_train[i])
+# for i in range(len(x_test)):
+#     x_test[i] = min_max_scaler(x_test[i])
+
 # Архитектура сети
 input_shape = (10, 100, 1)
-inputs = Input(shape=input_shape)
-x = Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
-x = BatchNormalization()(x)
-x = Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same')(x)
-outputs = Conv2D(1, kernel_size=(3, 3), activation='linear', padding='same')(x)
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=input_shape),
+    MaxPooling2D((2, 2), strides=1, padding='same'),
+    Conv2D(64, (3,3), padding='same', activation='relu'),
+    MaxPooling2D((2, 2), strides=1, padding='same'),
+    Conv2D(1, kernel_size=(3, 3), activation='linear'),
+    Flatten(),
+    Dense(1000, activation='linear'),
+    # Dense(10,  activation='softmax')
+])
 
-model = Model(inputs=inputs, outputs=outputs)
 model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001))
-model.fit(x_train, y_train, batch_size=512, epochs=3, validation_split=0.2)
+model.fit(x_train, y_train.reshape((len(y_train), 1000)), batch_size=512, epochs=3, validation_split=0.2)
 
-predicted_Y = model.predict(x_test)
+predicted_Y = model.predict(x_test).reshape((len(y_test), 10, 100))
 
 
 def reconstruct_matrix_super(matrix_target_shape, y, n=10, step_n=10, step_m=10):
@@ -61,19 +76,43 @@ def reconstruct_matrix_super(matrix_target_shape, y, n=10, step_n=10, step_m=10)
 
 
 
-rec1 = reconstruct_matrix_super(true_raw_shape, y_test)
+rec1 = reconstruct_matrix_super(true_raw_shape, x_test)
 rec2 = reconstruct_matrix_super(true_raw_shape, predicted_Y)
 
 
-plt.subplot(2, 1, 1)  # 2 строки, 1 столбец, подграфик 1
-plt.imshow(rec1, cmap='hot')  # График для первого подграфика
-plt.title('Subplot 1')  # Заголовок для первого подграфика
+def plot_with_slider(data):
+    def plot_threshold(threshold):
+        ax1 = plt.subplot(2, 1, 1)  # 2 строки, 1 столбец, подграфик 1
+        plt.imshow(rec1, cmap='hot')  # График для первого подграфика
+        thresholded_data = np.where(data > threshold, 1, 0)
+        plt.subplot(2, 1, 2, sharex=ax1, sharey=ax1)
+        plt.imshow(thresholded_data, cmap='gray')
+        plt.show()
 
-# Создание второго подграфика
-plt.subplot(2, 1, 2)  # 2 строки, 1 столбец, подграфик 2
-plt.imshow(rec2, cmap='hot')  # График для второго подграфика
-plt.title('Subplot 2')  # Заголовок для второго подграфика
+    # Создание окна с ползунком для выбора порогового значения
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.2)
+    threshold_slider_ax = plt.axes([0.2, 0.1, 0.65, 0.03])
+    threshold_slider = widgets.Slider(threshold_slider_ax, 'Threshold', 0, 1, valinit=0.5)
 
-# Отображение графиков
-plt.tight_layout()  # Автоматическое расположение подграфиков
-plt.show()
+    def update(val):
+        plot_threshold(threshold_slider.val)
+
+    threshold_slider.on_changed(update)
+    plt.show()
+
+
+# plt.subplot(2, 1, 1)  # 2 строки, 1 столбец, подграфик 1
+# plt.imshow(rec1, cmap='hot')  # График для первого подграфика
+# plt.title('Subplot 1')  # Заголовок для первого подграфика
+#
+# # Создание второго подграфика
+# plt.subplot(2, 1, 2)  # 2 строки, 1 столбец, подграфик 2
+# plt.imshow(rec2, cmap='hot')  # График для второго подграфика
+# plt.title('Subplot 2')  # Заголовок для второго подграфика
+#
+# # Отображение графиков
+# plt.tight_layout()  # Автоматическое расположение подграфиков
+# plt.show()
+
+plot_with_slider(rec2)
